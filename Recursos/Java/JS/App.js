1484d84b12809ac893e6f1df375297b47299f65c
Luis Zanella <luiscontreraszane@gmail.com>
2018-09-04 13:24:22 -0500

    User CRUD Added with Angular

---
+++ Recursos/Java/JS/App.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module("myApp", ["ngRoute"]);
+app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $routeProvider.when('/', { //Routing for show list of employee
+        templateUrl: '/UserList.html',
+        controller: 'UserController'
+    }).when('/AddUser', { //Routing for add employee
+        templateUrl: '/AddUser.html',
+        controller: 'UserController'
+        }).when('/UserList', { //Routing for add employee
+            templateUrl: '/UserList.html',
+            controller: 'UserController'
+        })
+        .when('/EditUser/:empId', { //Routing for geting single employee details
+            templateUrl: '/UpdateUser.html',
+            controller: 'UserController'
+        })
+        .when('/DeleteUser/:empId', { //Routing for delete employee
+            templateUrl: '/DeleteUser.html',
+            controller: 'UserController'
+        })
+        .otherwise({ //Default Routing
+            controller: 'UserController'
+        });
+}]);
---
+++ Recursos/Java/JS/UserController.js
@@ -0,0 +1,115 @@
+﻿var config = {
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+    }
+};
+app.controller("UserController", ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams)
+{
+    $scope.ListOfUsers;
+    $scope.Status;
+    //Get all employee and bind with html table
+    $http({
+        method: 'GET',
+        url: 'api/OtherUser'
+    }).then(function (response) {
+        console.warn(response);
+        $scope.ListOfUsers = response.data;
+    }, function (error) {
+        $scope.Status = "data not found";
+        alert("Error");
+    });
+    $scope.Close = function () {
+        $location.path('/UserList');
+    };
+
+
+    //Add new employee
+    $scope.Add = function () {
+        var UserData = {
+            Name: $scope.Name,
+            FLastName: $scope.FLastName,
+            SLastName: $scope.SLastName,
+            Nick: $scope.Nick,
+            Password: $scope.Password,
+            BirthDate: $scope.BirthDate
+            // DepartmentID: $scope.DepartmentID
+        };
+        $http({
+            method: 'Post',
+            url: 'api/OtherUser',
+            data: UserData,
+            config
+        }).then(function (response) {
+            $location.path('/UserList');
+            }, function (error) {
+                console.log(error.data);
+            $scope.error = "Something wrong when adding new employee " + data.ExceptionMessage;
+        });
+    };
+
+    //Fill the employee records for update
+
+    if ($routeParams.empId) {
+        $scope.Id = $routeParams.empId;
+        $http({
+            method: 'GET',
+            url: 'api/OtherUser/' + $scope.Id
+        }).then(function (response) {
+            $scope.Id = response.data.Id;
+            $scope.Name = response.data.Name;
+            $scope.FLastName = response.data.FLastName;
+            $scope.SLastName = response.data.SLastName;
+            $scope.Nick = response.data.Nick;
+            $scope.Password = response.data.Password;
+            $scope.BirthDate = response.data.BirthDate;
+        }, function (error) {
+            $scope.Status = "data not found";
+            alert("Error");
+        });
+    }
+
+    //Update the employee records
+    $scope.Update = function () {
+        var UserData = {
+            Id: $scope.Id,
+            Name: $scope.Name,
+            FLastName: $scope.FLastName,
+            SLastName: $scope.SLastName,
+            Nick: $scope.Nick,
+            Password: $scope.Password,
+            BirthDate: $scope.BirthDate
+            //DepartmentID: $scope.DepartmentID
+        };
+        UserData = JSON.stringify(UserData);
+        if ($scope.Id > 0) {
+            $http({
+                method: 'Put',
+                url: 'api/OtherUser/' + $scope.Id,
+                data: UserData,
+                config
+            }).then(function (response) {
+                $location.path('/UserList');
+            }, function (error) {
+                console.log(error.data);
+                $scope.error = "Something wrong when adding updating employee " + data.ExceptionMessage;
+            });
+        }
+    };
+
+
+    //Delete the selected employee from the list
+    $scope.Delete = function () {
+        if ($scope.Id > 0) {
+            $http({
+                method: 'Delete',
+                url: 'api/OtherUser/' + $scope.Id
+            }).then(function (response) {
+                $location.path('/UserList');
+            }, function (error) {
+                console.log(error.data);
+                $scope.error = "Something wrong when adding Deleting employee " + data.ExceptionMessage;
+            });
+        }
+
+    };
+}]);
